fb43df365ce073e326475f13a83f49c18f3af808
jinhyeokcif23 <jangj864@newschool.edu>
2023-11-13 23:55:02 -0500

    d

--- entries/entry8/final/script.js
+++ entries/entry8/final/script.js
@@ -9,11 +9,22 @@ $(document).ready(function() {
         // Determine the text to display based on the click count
         var textToShow = "";
         switch (clickCount) {
-            case 1: textToShow = "<p class='home-text'>home</p>"; break;
-            case 2: textToShow = "<p class='seven-eleven-text'>711</p>"; break;
-            case 3: textToShow = "<p class='cvs-text'>cvs</p>"; break;
-            case 4: textToShow = "<p class='weee-text'>weee</p>"; break;
-            case 5: textToShow = "<p class='parsons-text'>parsons</p>"; break;
+            case 1: textToShow = "<p class='home-text'><span>Hot Dried Pot</span> : Free Food. My friend spent his money.</p>"; 
+            break;
+            case 2:
+                textToShow = "<p class='seven-eleven-text'><span>Chicken</span> : The chicken has no meat, only batter, and it tastes bad.</p>" +
+                             "<p class='seven-eleven-text2'><span>Doritos</span> : Nacho cheese is the second-best flavor.</p>";
+                break;
+            case 3: 
+                textToShow = "<p class='cvs-text'><span>Menthol Spray</span> : It's Chloraseptic® Sore Throat Medicine.</p>" +
+                             "<p class='cvs-text'><span>C4® Energy Drink</span> : Dynamic taste and explosive energy to enhance performance.</p>";
+                break;
+            case 4: 
+                textToShow = "<p class='weee-text'><span>Cooked White Rice</span> : Essential supplies for securing food this month.</p>" +
+                             "<p class='weee-text2'><span>Stir-Fried Topokki</span> : Delicious but detrimental to diet.</p>"; 
+                
+                break;
+            case 5: textToShow = "<p class='parsons-text'><span>TNS Restaurants</span> : I ordered a pepperoni pizza, but although I was very hungry, the size was too small.</p>"; break;
             case 6:
                 textToShow = "<p class='refresh-text'>Refresh to Restart!</p>";
                 // Move the block back to its original position
@@ -34,14 +45,15 @@ $(document).ready(function() {
                 // If destination is greater than 1320px, reset to 10px
                 $("#block").animate({
                     marginLeft: "10px"
-                }, 500); // Changed duration to 500 to match the reset animation
+                }, 500);
             } else {
                 // Otherwise, move to the new destination
                 $("#block").animate({
                     marginLeft: dest + "px"
-                }, 500); // Changed duration to 500 for consistency
+                }, 500);
             }
         }
     });
 });
+
 
