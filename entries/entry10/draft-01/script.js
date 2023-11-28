0935336d36abed32562e67f0d96d15e7b8f742e9
jangj864 <jangj864@newschool.edu>
2023-11-28 09:45:06 -0500

    d

--- entries/entry10/draft-01/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var button = document.getElementById('actionButton');
-    var output = document.getElementById('output');
-
-    button.addEventListener('click', function() {
-        output.textContent = 'You clicked the button!';
-    });
-});
-
